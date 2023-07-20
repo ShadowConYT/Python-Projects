ed6b8331e61edfb0e64005f2102380308cd7cb91
ShadowConYT <iamajay6122@gmail.com>
2023-07-20 22:57:19 +0530

    Hangman Algorithm

--- Hangman/hangMan.py
+++ Hangman/hangMan.py
@@ -1,5 +1,23 @@
 from random import choice
+'''
+    Logic: 
 
+    Hangman is a Word Guessing Game where you will find the word by letters
+    if the letter you provided is the letter presents in the actual word
+    it will fill it up in the exact place where is the letter in that word 
+
+    Example : if the word is "apple" and your input is "p" then the output look like this
+    "_pp__" with this you can find what's the letter left and fill it up and win the game
+
+    Algorithm:
+
+    Step 1 : Take Input from the User 
+    Step 2 : Create an Empty Variable
+    Step 3 : then loop through the Actual word
+    Step 4 : if the letter entered presents in the word then append it to the Empty Variable
+    Step 5 : If not present add "_" instead of Letter.
+    Step 6 : Print out all the characters using Blank space between them as shown below example
+'''
 def runGame():
     word : str = choice(['apple','banana','carrot'])
     userName: str = input("What is your Name ? >>")
