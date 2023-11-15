ffe08c589636588afdb24c65e93d3729a1a79bd8
ShadowConYT <iamajay6122@gmail.com>
2023-11-15 23:56:34 +0530

    Fix GUI layout and add input fields for income and tax values

--- Tax-Calculator/Tax-Calculator.py
+++ Tax-Calculator/Tax-Calculator.py
@@ -4,16 +4,22 @@ class TaxCalculator:
     def __init__(self):
         self.window = ctk.CTk()
         self.window.title("Tax Calculator")
-        self.window.geometry("280*200")
-        self.window.resizable(True,True)
+        self.window.geometry("280x200")
+        self.window.resizable(False,False)
 
         self.padding: dict ={'padx':20, 'pady':10}
 
-        self.lblIncome = ctk.CTkLabel(self.window, text='Income :')
+        # Income Label and Entry
+        self.lblIncome = ctk.CTkLabel(self.window, text='Income:')
         self.lblIncome.grid(row=0, column=0, **self.padding)
+        self.income_label_entry = ctk.CTkEntry(self.window, placeholder_text="Enter Appropriate Income")
+        self.income_label_entry.grid(row=0, column=1, **self.padding)
 
-        self.income_label_entry = ctk.CTkEntry(self.window)
-        self.income_label_entry.grid(row=0, column=0, **self.padding)
+        # Tax Label and Entry
+        self.taxLabel = ctk.CTkLabel(self.window, text="Tax:")
+        self.taxLabel.grid(row=1,column=0, **self.padding)
+        self.taxEntry = ctk.CTkEntry(self.window, placeholder_text="Enter Appropriate Value")
+        self.taxEntry.grid(row=1,column=1, **self.padding)
 
     def run(self):
         self.window.mainloop()
