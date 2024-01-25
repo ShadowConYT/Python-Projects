c273f0f35ec625ef4d02229dc4498020d850c9dd
ShadowConYT <iamajay6122@gmail.com>
2024-01-25 23:23:32 +0530

    Shipping Cost Added

--- Intermediate/Geo_Distance_Calculator/main.py
+++ Intermediate/Geo_Distance_Calculator/main.py
@@ -32,10 +32,24 @@ def get_distance(origin, destination):
         print("Invalid address")
         return None
 
+def shipping_cost(distance):
+    if distance <= 100:
+        return distance * 3.5
+    elif distance <= 500:
+        return distance * 4.0
+    elif distance <= 1000:
+        return distance * 4.5
+    elif distance > 1000:
+        return distance * 5.0
+    else:
+        return None
+
 def main():
     origin = "Chennai, Tamil Nadu, India"
     destination = "Mumbai, Maharashtra, India"
-    get_distance(origin, destination)
+    distance = get_distance(origin, destination)
+    cost = shipping_cost(distance)
+    print(f"Shipping cost from {origin.split(', ')[0]} to {destination.split(', ')[0]} is {cost:.2f} INR")
     
 if __name__ == "__main__":
     main()
