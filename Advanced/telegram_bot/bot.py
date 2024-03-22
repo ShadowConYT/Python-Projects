b6db8e962b8dec5bd5553230fde1246437455ccd
ShadowConYT <iamajay6122@gmail.com>
2024-03-22 23:08:05 +0530

    Few Functionality updations in TeleBot

--- Advanced/telegram_bot/bot.py
+++ Advanced/telegram_bot/bot.py
@@ -14,9 +14,9 @@ async def start_message(update: Update, context: ContextTypes):
 async def help_message(update: Update, context: ContextTypes):
     await update.message.reply_text(f"Go Fuck Yourself!")
 
-async def random_num(update: Update, context: ContextTypes):
-    random = random.randint(0,10)
-    await update.message.reply_text(random)
+async def random_num(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    randNum = random.randint(0,10)
+    await update.message.reply_text(randNum)
 
 def handle_response(text: str):
     processed: str = text.lower()
@@ -33,7 +33,7 @@ async def handle_messages(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     print(f'User {update.message.chat.id} : {text} ({message_type})')
 
-    if message == 'group':
+    if message_type == 'group':
         if BOT_NAME in text:
             new_text = text.replace(BOT_NAME, '').strip()
             response = handle_response(new_text)
@@ -56,6 +56,8 @@ def main():
 
     # commands handler
     app.add_handler(CommandHandler('start', start_message))
+    app.add_handler(CommandHandler('help', help_message))
+    app.add_handler(CommandHandler('random', random_num))
 
     # Message Handler
     app.add_handler(MessageHandler(filters.TEXT, handle_messages))
